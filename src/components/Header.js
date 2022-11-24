19efb6fa5b708f08acc4bbba2bfc66390a0e9781
mpham-12 <martinp0512@gmail.com>
2022-11-24 12:37:28 -0500

    add header bio content

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,15 @@ import video from '../assets/video.mp4'
 import classes from './Header.module.css'
 
 const Header = () => {
-  return ( 
+  return (
     <div className={classes.header}>
-      <video src={video} autoPlay loop muted/>
+      <video src={video} autoPlay loop muted />
+      <div className={classes.content}>
+        <p>Hey! I'm Martin Pham, a Front-end Web Developer from Toronto, Canada.</p>
+        <p>I have a passion for creating captivating web-applications through meaningful and engaging user-experiences.</p>
+      </div>
     </div>
-   );
+  );
 }
- 
+
 export default Header;
