5774311dbfdf006cfad5efc0d1a2e759b7467a76
mpham-12 <martinp0512@gmail.com>
2022-11-24 12:56:35 -0500

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header'
+import SideBar from './components/SideBar'
 
 function App() {
   return (
     <div className="App">
-<Header/>
+      <Header />
+      <Header />
+      <Header />
+      <SideBar />
     </div>
   );
 }
---
+++ src/components/SideBar.js
@@ -0,0 +1,16 @@
+import classes from './SideBar.module.css'
+
+const SideBar = () => {
+  return (
+    <div className={classes.sidebar}>
+      <img src="./mail.png" alt="" />
+      <img src="./mail.png" alt="" />
+      <img src="./mail.png" alt="" />
+      <img src="./mail.png" alt="" />
+      <img src="./mail.png" alt="" />
+      
+    </div>
+  );
+}
+
+export default SideBar;
