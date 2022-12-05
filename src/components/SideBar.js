fb16d17aa8d7f660c24588401aefcb6129f2beb1
mpham-12 <martinp0512@gmail.com>
2022-12-05 17:08:48 -0500

    add anchor tag links to sidebar icons

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,10 +4,10 @@ const SideBar = () => {
   return (
     <div className={classes.sidebar}>
 
-      <img className={classes.icon} src="github.png"/>
-      <img className={classes.icon} src="linkedin.png"/>
-      <img className={classes.icon} src="mail.png"/>
-      <img className={classes.icon} src="resume.png"/>
+      <a href="http://www.github.com/mpham-12"><img className={classes.icon} src="github.png"/></a>
+      <a href="https://www.linkedin.com/in/martin-pham-dev/"><img className={classes.icon} src="linkedin.png"/></a>
+      <a href="mailto:martinpham.dev@gmail.com"><img className={classes.icon} src="mail.png"/></a>
+      <a href="https://resume.creddle.io/resume/11h9vyikajf"><img className={classes.icon} src="resume.png"/></a>
 
     </div>
   );
