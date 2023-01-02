0200cb48ef8deedbdd4214085709832c1bc59d1b
mpham-12 <martinp0512@gmail.com>
2023-01-02 18:46:14 -0500

    attempt to play video on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import classes from './Header.module.css'
 const Header = () => {
   return (
     <div className={classes.header}>
-      <video src={video} playsinline autoplay muted loop/>
+      <video src={video} autoplay="autoplay" loop="loop" muted defaultMuted playsinline  oncontextmenu="return false;"  preload="auto"  id="myVideo"/>
       <div className={classes.content}>
         <p className={classes.contentp}>Hello, I'm Martin Pham.</p>
         <p className={classes.contentp}>I'm a Front-end Web Developer based in Toronto, Canada.</p>
