ecb5d3f04704ed3568b3fd8e2fba11d270ac3b0c
mpham-12 <martinp0512@gmail.com>
2023-01-02 17:53:11 -0500

    make changes for netlify deployment

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -5,9 +5,9 @@ const SideBar = () => {
     <div className={classes.sidebar}>
 
       <a href="http://www.github.com/mpham-12" target='_blank' rel="noreferrer"><img className={classes.icon} src="github.png" alt=""/></a>
-      <a href="https://www.linkedin.com/in/martin-pham-dev/" target='_blank'><img className={classes.icon} src="linkedin.png" alt=""/></a>
-      <a href="mailto:martinpham.dev@gmail.com" target='_blank'><img className={classes.icon} src="mail.png" alt=""/></a>
-      <a href="https://resume.creddle.io/resume/11h9vyikajf" target='_blank'><img className={classes.icon} src="resume.png" alt=""/></a>
+      <a href="https://www.linkedin.com/in/martin-pham-dev/" target='_blank' rel="noreferrer"><img className={classes.icon} src="linkedin.png" alt=""/></a>
+      <a href="mailto:martinpham.dev@gmail.com" target='_blank' rel="noreferrer"><img className={classes.icon} src="mail.png" alt=""/></a>
+      <a href="https://resume.creddle.io/resume/11h9vyikajf" target='_blank' rel="noreferrer"><img className={classes.icon} src="resume.png" alt=""/></a>
 
     </div>
   );
