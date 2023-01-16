65920abed9385ed7571bc5eb3eecb0fad0e9a015
mpham-12 <martinp0512@gmail.com>
2023-01-16 10:15:10 -0500

    update live links and fix project description for movie app

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,10 +5,10 @@ const projects = [
   {
     title: 'PluggedIn',
     image: './movie.png',
-    description: "PluggedIn is an app that displays updated movie information using TMDB's API. Users can choose a movie to find out general information such as the release date, synopsis, and rating. In addition, users can filter through movies by genre and view categories such as Trending and Most Popular, which are constantly being updated.",
+    description: "PluggedIn is an app that displays updated movie information using TMDB's API. Users can choose a movie to find out general information such as the release date, synopsis, and rating. In addition, users can filter through movies by genre and view categories such as Trending and Most Popular, which are constantly being updated. *Before running this application, please visit https://cors-anywhere.herokuapp.com/corsdemo and allow temporary access to the server.*",
     techStack: 'HTML, CSS, Javascript, React, React Router, The Movie DB API',
     githubUrl: 'https://github.com/mpham-12/movie-app',
-    liveUrl: '',
+    liveUrl: 'https://velvety-douhua-c0fc98.netlify.app',
   },
   {
     title: 'Chat Time',
@@ -16,7 +16,7 @@ const projects = [
     description: 'Chat Time is an instant-messaging app that allows friends and strangers to chat in real time. Simply sign up and select a user to begin chatting.',
     techStack: 'HTML, CSS, Javascript, React, React Router, Firebase',
     githubUrl: 'https://github.com/mpham-12/chat-time',
-    liveUrl: '',
+    liveUrl: 'https://bright-crisp-85c3a6.netlify.app',
   },
   {
     title: 'Rest & Press',
@@ -24,16 +24,8 @@ const projects = [
     description: 'Rest & Press is a front-end juice delivery app that offers organic cold-press juices. Users choose juices to add to their cart and submit their order after entering their delivery information into a form.',
     techStack: 'HTML, CSS, Javascript, React, Firebase',
     githubUrl: 'https://github.com/mpham-12/green-juice-app',
-    liveUrl: '',
-  },
-  {
-    title: 'Day Trip',
-    image: './daytrip.png',
-    description: '',
-    techStack: '',
-    githubUrl: '',
-    liveUrl: '',
-  },
+    liveUrl: 'https://cerulean-frangipane-fc82e0.netlify.app',
+  }
 ]
 
 const Projects = () => {
